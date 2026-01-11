53c749276f02dcdc634d5b7908ebe319859578fc
vAAAAk <farouk.sallak@outlook.com>
2026-01-11 13:37:43 +0300

    Added procedures to check and validate booking inserts
    Signed-off-by: vAAAAk <farouk.sallak@outlook.com>

---
+++ stored_procedures/AddValidBooking.sql
@@ -0,0 +1,15 @@
+CREATE DEFINER=`farouk_elsallak`@`%` PROCEDURE `AddValidBooking`(IN date_check DATETIME, IN table_number INT, IN customer_number INT)
+BEGIN
+	DECLARE booking_count INT;
+    
+	START TRANSACTION;
+    INSERT INTO bookings (booking_date, table_id, customers_customer_id) VALUES (date_check, table_number, customer_number);
+    SELECT COUNT(*) INTO booking_count FROM bookings WHERE booking_date = date_check AND table_id = table_number;
+    IF booking_count > 1 THEN 
+		ROLLBACK;
+		SELECT CONCAT('Table ', table_number,' is already booked - transaction cancelled') AS Status;
+	ELSE
+		COMMIT;
+        SELECT CONCAT('Table ', table_number,' booked successfully - transaction completed') AS Status;  
+    END IF;
+END
